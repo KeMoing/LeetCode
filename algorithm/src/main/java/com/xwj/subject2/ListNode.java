5d4b9addf7f1dfc31af747a66dc21edc7fe19c82
KeMoing <1325628058@qq.com>
2020-06-30 00:22:22 +0800

    增加LeetCode题2：两数相加

---
+++ algorithm/src/main/java/com/xwj/subject2/ListNode.java
@@ -0,0 +1,16 @@
+package com.xwj.subject2;
+
+/**
+ * Definition for singly-linked list.
+ *
+ * @author xuwenjie
+ * @create 2020-06-29 0:03
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ algorithm/src/main/java/com/xwj/subject2/Solution.java
@@ -0,0 +1,70 @@
+package com.xwj.subject2;
+
+
+/**
+ * @author xuwenjie
+ * @create 2020-06-29 0:01
+ */
+public class Solution {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode currNode = dummyHead;
+        ListNode pointer1 = l1;
+        ListNode pointer2 = l2;
+        int count = 0;
+        while (pointer1 != null || pointer2 != null) {
+            int num1 = pointer1 !=null ? pointer1.val : 0;
+            int num2 = pointer2 !=null ? pointer2.val : 0;
+            int sum = num1 + num2 + count;
+            count = sum / 10;
+
+            currNode.next = new ListNode(sum % 10);
+            currNode = currNode.next;
+
+            if (pointer1 != null) {
+                pointer1 = pointer1.next;
+            }
+            if (pointer2 != null) {
+                pointer2 = pointer2.next;
+            }
+
+        }
+        if (count != 0) {
+            currNode.next = new ListNode(count);
+        }
+
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        ListNode l1 = new ListNode(1);
+        ListNode next1 = new ListNode(4);
+        ListNode next2 = new ListNode(3);
+        l1.next = next1;
+        next1.next = next2;
+
+        ListNode l2 = new ListNode(5);
+        next1 = new ListNode(6);
+        next2 = new ListNode(4);
+        l2.next = next1;
+        next1.next = next2;
+
+        ListNode resList = solution.addTwoNumbers(l1, l2);
+        solution.printList(resList);
+    }
+
+    private void printList(ListNode listNode) {
+        while (listNode != null) {
+            System.out.print(listNode.val);
+            listNode = listNode.next;
+            if (listNode != null) {
+                System.out.print(" -> ");
+            }
+        }
+        System.out.println();
+    }
+
+}
